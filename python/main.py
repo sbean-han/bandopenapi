037f3a5e4d162f469af7236f0f102f8c275c6194
sungju.kwon <sungju.kwon@navercorp.com>
2023-10-21 10:52:37 +0900

    :rocket: initial commit

---
+++ python/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import src.bandopenapi.client as client
+
+
+def main():
+    print('hi')
+    api = client.BandOpenApi()
+    api.call()
+
+
+if __name__ == "__main__":
+    main()
+
